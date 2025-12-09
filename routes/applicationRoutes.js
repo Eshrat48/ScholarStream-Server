ff282190accbdec0f0523656e3c21dd973b57cf5
Eshrat Kamal Nova <eshratkamalnova@gmail.com>
2025-12-09 22:32:24 +0600

    Add route files structure

---
+++ routes/applicationRoutes.js
@@ -0,0 +1,8 @@
+// routes/applicationRoutes.js
+
+const express = require('express');
+const router = express.Router();
+
+// This router will be configured in index.js after controllers are initialized
+
+module.exports = router;
---
+++ routes/paymentRoutes.js
@@ -0,0 +1,8 @@
+// routes/paymentRoutes.js
+
+const express = require('express');
+const router = express.Router();
+
+// This router will be configured in index.js after controllers are initialized
+
+module.exports = router;
